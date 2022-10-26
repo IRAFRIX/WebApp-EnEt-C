953dd6f6a3f55df30c61b4e41bdbeff1f2e5d892
Nattatida <Nattatida.ff@gmail.com>
2022-10-26 08:34:22 +0700

    more Boot

--- index.php
+++ index.php
@@ -53,32 +53,34 @@
     <div class="container">
     <H1 style="text-align: center ;">WAKER WEBBROAD</H1>
     <?php include "nav.php"; ?>
-    หมวดหมู่:
-    <select name="thing">
-        <option value="All">--ทั้งหมด--</option>
-        <option value="Normal">เรื่องทั่วไป</option>
-        <option value="Learn">เรื่องเรียน</option>
-    </select>
-    <div style="float:right ;">
-        <?php
-        echo "ผู้ใช้งานระบบ:".$_SESSION['username'];
-        ?>
-        &nbsp;&nbsp;
-        <a href="logout.php">ออกจากระบบ</a>
+    <div class="d-flex justify-content-between">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="dropdown">
+                <button class="btn btn-light dropdown-toggle btn-sm"
+                type="button" id="button2" data-bs-toggle="dropdown"
+                aria-expanded="false">--ทั้งหมด--</button>
+                <ul class="dropdown-menu" aria-labelledby="button2">
+                    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+                </ul>
+            </span>
+        </div>
+        <div><a href="newpost.php" class="btn btn-success btn-sm"><i class="bi bi-plus-lg"></i> ทำการสร้างกระทู้ใหม่</a></div>
     </div>
     <br>
-    <a href="newpost.php">สร้างกระทู้ใหม่</a>
-    <ul>
+    <table class="table table-striped">
         <?php
             for($i=1;$i<=10;$i++){
-                echo "<li>";
-                echo "<a href=post.php?id="."$i".">กระทู้ที่ ".$i."</a>";
-                if($_SESSION['role'] == "a"){
-                    echo "&nbsp;&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+                echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้ที่ ".$i."</a></td>";
+                if ($_SESSION['role'] =='a'){
+                    echo"<td><a href=delete.php?id=$i class='btn btn-danger btn-sm'><i class='bi bi-trash'></i></a></td>";
                 }
-                echo "</li>";
+                echo"</tr>";
             }
         ?>
+    </table>
     </ul>
     </div>
     </div>
